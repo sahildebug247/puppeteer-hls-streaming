1c065ff131e4eae79632bff221b1cbfcbc208e11
Sahil Yadav <sahil.yadav21@yahoo.com>
2020-07-14 20:53:25 +0530

    making changes to video util

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,7 +1,36 @@
-import { webm2Mpegts } from '../utils/video.util';
+import {
+  webm2Mpegts,
+  findVideoDuration,
+  generateM3u8Playlist,
+} from '../utils/video.util';
 class AppService {
   public async handleFileUpload(file: any) {
-    await webm2Mpegts(file.path);
+    const webmFilePath = file.path;
+
+    const tsFilePath = webmFilePath
+      .replace('webm', 'ts')
+      .replace('puppeteer-uploads', 'streaming-output');
+
+    const isFirst = file.path.includes('/1.webm');
+    await webm2Mpegts(webmFilePath, tsFilePath);
+
+    const playlistPath = 'assets/streaming-output/playlist.m3u8';
+    const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    await sleep(1000);
+    findVideoDuration(tsFilePath, (err, duration) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      duration = duration.toFixed(2);
+      generateM3u8Playlist(
+        duration,
+        file.filename.replace('webm', 'ts'),
+        playlistPath,
+        tsFilePath,
+        isFirst
+      );
+    });
     console.log({ file });
   }
 }
--- src/utils/video.util.ts
+++ src/utils/video.util.ts
@@ -1,19 +1,48 @@
 const child_process = require('child_process');
-const FFMPEG_DIR = '/usr/bin/ffmpeg';
+const FFMPEG_DIR = '/usr/bin/';
+const fs = require('fs');
 
-// o {fp, t0}
-async function webm2Mpegts(filePath: string) {
+async function findVideoDuration(fp, cb) {
+  let out: any = [];
+
+  const args = [
+    FFMPEG_DIR + 'ffprobe',
+    '-v',
+    'quiet', // less verbose
+    '-print_format',
+    'json', // output json
+    '-show_format', // return format info
+    // '-show_streams',
+    fp,
+  ];
+
+  const cmd = args.shift();
+
+  const proc = child_process.spawn(cmd, args);
+
+  proc.stdout.on('data', function (data) {
+    out.push(data.toString());
+  });
+
+  proc.on('close', function () {
+    //console.log('close');
+    out = out.join('');
+
+    out = JSON.parse(out);
+    console.log({ out });
+    const duration = parseFloat(out.format.duration);
+    cb(null, duration);
+  });
+}
+async function webm2Mpegts(filePath: string, tsFilePath: string) {
   let out: any = [];
   let err: any = [];
   const fps = 30;
   //   filePath = `../../` + filePath;
-  const newFilePath = filePath
-    .replace('webm', 'ts')
-    .replace('puppeteer-uploads', 'streaming-output');
 
-  console.log({ filePath, newFilePath });
+  console.log({ filePath, tsFilePath });
   const args = [
-    FFMPEG_DIR,
+    FFMPEG_DIR + 'ffmpeg',
     '-v',
     'quiet', // less verbose
     '-loglevel',
@@ -40,7 +69,7 @@ async function webm2Mpegts(filePath: string) {
     // 'setpts=PTS+' + t0 + '/TB',
     //-async 1
     '-y', // overwrite output files
-    newFilePath, // output file
+    tsFilePath, // output file
   ];
 
   //console.log(args.join(' '));
@@ -66,9 +95,46 @@ async function webm2Mpegts(filePath: string) {
     if (err) {
       console.log('\nERR\n' + err + '\n');
     }
-
-    console.log('conversion done');
   });
 }
 
-export { webm2Mpegts };
+async function generateM3u8Playlist(
+  duration,
+  fileName,
+  playlistPath,
+  tsFilePath,
+  isFirst
+) {
+  let meta;
+  if (isFirst) {
+    meta = [
+      '#EXTM3U',
+      '#EXT-X-VERSION:3',
+      '#EXT-X-MEDIA-SEQUENCE:0',
+      '#EXT-X-ALLOW-CACHE:YES',
+      '#EXT-X-TARGETDURATION:' + duration,
+    ];
+    meta.push('#EXTINF:' + duration + ',');
+    meta.push(fileName);
+    meta.push('');
+    meta = meta.join('\n');
+    fs.writeFile(playlistPath, meta, () => {
+      console.log('playlist file created');
+    });
+  } else {
+    meta = [];
+    meta.push('#EXTINF:' + duration + ',');
+    meta.push(fileName);
+    meta.push('');
+    meta = meta.join('\n');
+    fs.appendFile(playlistPath, meta, () => {
+      console.log('playlist file appended');
+    });
+  }
+
+  // if (!isLive) {
+  //   meta.push('#EXT-X-ENDLIST');
+  // }
+}
+
+export { webm2Mpegts, findVideoDuration, generateM3u8Playlist };
